1e0c14b83749237c094bb2f21a5c93bca1bb2527
Aakkash24 <skybully07@gmail.com>
2022-10-23 14:01:37 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusbar from "./FocusedStatusbar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
+export {
+  HomeHeader,
+  FocusedStatusbar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
